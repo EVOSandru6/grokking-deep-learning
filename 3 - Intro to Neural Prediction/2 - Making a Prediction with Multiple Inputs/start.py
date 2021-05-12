846964a0822857ba13a9953b0e392a10ca636520
EVOSandru6 <evosduple@mail.ru>
2021-05-12 22:55:45 +0300

    first steps

---
+++ 3 - Intro to Neural Prediction/2 - Making a Prediction with Multiple Inputs/start.py
@@ -0,0 +1,20 @@
+def w_sum(inputs, weights):
+    assert (len(weights) == len(weights))
+    output = 0
+    for i in range(len(inputs)):
+        output += input[i] * weights[i]
+    return output
+
+def neural_network(inputs, weights):
+    pred = w_sum(inputs, weights)
+    return pred
+
+weights = [.2, .3, 0]
+
+toes = [8.5, 9.5, 9.9, 9.0]
+wlrec = [.65, .8, .8, .9]
+nfans = [1.2, 1.3, .5, 1]
+
+input = [toes[0], wlrec[0], nfans[0]]
+
+pred = neural_network(input, weights)
