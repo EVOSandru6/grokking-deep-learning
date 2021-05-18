eebdb66547c06040aa94201568baf65ff8620afa
EVOSandru6 <evosduple@mail.ru>
2021-05-18 09:13:58 +0300

    add numpy example

---
+++ 3 - Intro to Neural Prediction/8 - A Quick Primer on NumPy/start.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+a = np.array([0, 1, 2, 3])  # a vector
+b = np.array([4, 5, 6, 7])  # another vector
+c = np.array([
+    [0, 1, 2, 3],  # a matrix
+    [4, 5, 6, 7]
+])
+
+d = np.zeros((2, 4))
+e = np.random.rand(2, 5)
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
